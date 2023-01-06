ac03b3fbdaf2faf68daa6abcdc07ff34acad94b6
Julio Gtz <julio@perfilan.com>
2023-01-05 21:31:47 -0700

    funcion para enviar mensaje

--- main.py
+++ main.py
@@ -1,21 +1,34 @@
 from fastapi import FastAPI, Form
 from twilio.rest import Client
+import os
 
 app = FastAPI()
-TWILIO_ACCOUNT_SID = ''
-TWILIO_AUTH_TOKEN = ''
+
 
 @app.post("/")
 async def root(Body: str = Form(), From: str = Form(...)):
-    twilio_client = Client(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
-    from_whatsapp_number = 'whatsapp:+14155238886'
-    twilio_client.messages.create(body='Your {{1}} code is {{2}}', from_=from_whatsapp_number, to=From)
-    message = Body.lower()
-    phone_no = From.replace('whatsapp:', '')
-    print(message, phone_no)
-    return {'resp':'hi'}
+    # obtener mensaje de whatsapp
+    input_message = Body.lower()
+    # numero de whatsapp
+    input_number = From.replace('whatsapp:', '')
+    # detectar si el número existe en la base de datos.
 
+    send_message_twilio(From, 'esto es un mensaje para ti: '+From)
 
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+    return {
+        'input_message': input_message,
+        'input_number': input_number
+    }
+
+def send_message_twilio(to_number, message):
+    twilio_config = {
+        'sdi': os.environ['TWILIO_ACCOUNT_SID'],
+        'token': os.environ['TWILIO_TOKEN'],
+        'number': 'whatsapp:+14155238886'
+    }
+    twilio_client = Client(twilio_config['sdi'], twilio_config['token'])
+    try:
+        twilio_client.messages.create(body=message, from_=twilio_config['number'], to=to_number)
+        return 'mensaje enviado a '+to_number+':, '+message
+    except Exception as e:
+        return 'error al enviar el mensaje: '+e
