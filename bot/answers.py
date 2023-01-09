2dc91fe2aaee5f56c476e06e3cec9fc5dd7fda56
Julio Gtz <julio@perfilan.com>
2023-01-08 21:22:35 -0700

    save

--- bot/answers.py
+++ bot/answers.py
@@ -2,7 +2,7 @@ def get_answers(key, language):
 
     response = {
         'welcome': {
-            'es': "Hola bienvenido a savelink.io, we have created an account with your phone: %s",
+            'es': "Hola bienvenido a savelink.io, hemos creando una cuenta con tu número: %s",
             'en': 'Welcome to savelink.io, we have created an account with your phone: %s',
 
         }
